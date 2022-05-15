a32df2cf909bbb8a3d588aa626e6df15c2b49e45
Richard Clarke <freshnewrich@gmail.com>
2022-05-15 14:40:28 -0700

    about

---
+++ src/pages/About.jsx
@@ -0,0 +1,8 @@
+export default function About() {
+    return (
+        <div className="home">
+            <h3>"What is there to say, finally, except that pain is bad and pleasure good, life all, death nothing.”</h3>
+            <h3>- Gore Vidal</h3>
+        </div>
+    )
+}
