6f99328d5bf520dd37fa16299a4a456f93f4a0e1
Richard Clarke <freshnewrich@gmail.com>
2022-05-15 13:58:11 -0700

    app.js imports and routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import {Route, Routes} from 'react-router-dom'
+
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Stock from "./pages/Stock"
+import Stocks from "./pages/Stocks"
+
+// {/* Route	Renders	Component
+// /	"This is the Homepage page"	Home
+// /about	"This is theAabout page"	About
+// /stocks/:symbol	A single stock	Stock
+// /stocks	All stocks	Dashboard */}
 
 function App() {
   return (
     <div className="App">
-     
-{/* Route	Renders	Component
-/	"This is the Homepage page"	Home
-/about	"This is theAabout page"	About
-/stocks/:symbol	A single stock	Stock
-/stocks	All stocks	Dashboard */}
+<Routes>
+<Route path="/" element={<Home/>}/>
+<Route path="/about" element={<About/>}/>
+<Route path="/stocks" element={<Stocks/>}/>
+<Route path="/stocks/:symbol" element={<Stock/>}/>
+</Routes>
     </div>
   );
 }
