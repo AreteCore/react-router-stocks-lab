dec6783bf90262aa8712db7c68e8d148e9a30189
Richard Clarke <freshnewrich@gmail.com>
2022-05-15 23:26:29 -0700

    it's still ugly

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,7 +2,7 @@ import {Link} from "react-router-dom"
 
 export default function Nav() {
    return <div className="nav">
-        <h3>Navigation</h3>
+        {/* <h3>Navigation</h3> */}
         <Link to={"/"}><div>Home/Portfolio</div></Link>
         {/* <Link to={"/stocks"}><div>My Portfolio</div></Link> */}
         <Link to={"/about"}><div>About Page</div></Link>
