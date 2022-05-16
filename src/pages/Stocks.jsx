f0b7f9c2f4a3b27f8c9449ffd34987ed61b106ba
Richard Clarke <freshnewrich@gmail.com>
2022-05-15 20:50:08 -0700

    stocks.jsx

--- src/pages/Stocks.jsx
+++ src/pages/Stocks.jsx
@@ -1,6 +1,23 @@
-import data from "../data.js"
+import {Link} from "react-router-dom"
+import stocks from "../data.js"
 
 export default function Stocks() {
-console.log(data)
-return "stocks"
+// console.log(stocks)
+// stocks.forEach(stock => console.log(stock.name))
+return (
+    <div className="stocks">
+        <h1>My Stock Portfolio</h1>
+        {stocks.map((stock, index) => {
+            return (
+                    <Link 
+                    key={stock.symbol} 
+                    to={`/stock/${stock.symbol}`}>
+                        {stock.name}
+                    </Link>
+            )
+        } )
+    }
+    </div>
+    
+)
 }
